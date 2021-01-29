0240daae6d103b8acf12374922c7e7d4ac840899
Kozulina Nataliya <natasha.pechenka@gmail.com>
2021-01-29 18:56:09 +0600

    added prime number handling

--- 1.3.a.py
+++ 1.3.a.py
@@ -5,9 +5,12 @@ given_number = 0
 while given_number < 2:
     given_number = int(input("Enter a number: "))
 while True:
-    for i in range(1,given_number):
-        if given_number%i == 0 and i != 1:
+    for i in range(1,given_number+1):
+        if given_number%i == 0 and i != 1 and given_number != i:
             print("The smallest divisor of ",given_number," is ",i)
             break
-        
+        elif given_number%i == 0 and i == given_number:
+            print("You\'ve entered A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers.")
+            print(given_number)
+            break   
     break
